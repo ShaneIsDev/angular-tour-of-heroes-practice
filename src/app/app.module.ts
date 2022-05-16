5cb2cd11988dc3cceeb719b2052331ec0f432115
Shane Brown <buithinh.tg@gmail.com>
2022-05-16 17:53:50 +0700

    finished entire tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service'
+import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component'
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { InMemoryDataService } from './in-memory-data.service'
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -18,7 +18,7 @@ export class HeroService {
     );
   }
   getHero(id: number): Observable<Hero> {
-    const url= `${this.heroesUrl}/${id}`;
+    const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fletched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
@@ -32,18 +32,39 @@ export class HeroService {
     this.messageService.add(`HeroService: ${message}`);
   }
   private heroesUrl = 'api/heroes';
-  private handleError<T>(operation = 'operation', result?: T){
-    return (error:any):Observable<T> =>{
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
       console.error(error);
       this.log(`${operation} failed: ${error.message}`);
       return of(result as T);
     }
   }
-  updateHero(hero:Hero):Observable<any>{
-    return this.http.put(this.heroesUrl,hero,this.httpOptions).pipe(tap(_ => this.log(`updated hero id=${hero.id}`)),
-    catchError(this.handleError<any>("updateHero")));
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>("updateHero")));
   }
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero id=${newHero.id}`)), catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions)
+      .pipe(tap(_ => this.log(`Deleted hero id=${id}`)), catchError(this.handleError<Hero>('deleteHero')));
+  }
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ? this.log(`Hero Found!`) : this.log(`Hero not Found!`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
 }
